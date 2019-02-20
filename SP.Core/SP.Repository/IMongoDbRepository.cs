5af170842996e9a1dc9cb8200532458fcb7820fa
yemobai <546307885@qq.com>
2019-02-20 23:16:13 +0800

    添加mongodb接口

---
+++ SP.Core/SP.Repository/IMongoDbRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Repository
+{
+    interface IMongoDbRepository<TEntity> where TEntity:class,new()
+    {
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync();
+        Task QueryAsync();
+        Task DeleteAsync();
+    }
+}
